34e9609405d78e8a0a431c0cb752b1c2b177a8c1
lixiang <1850094299@qq.com>
2021-05-07 10:07:22 +0800

    1486. 数组异或操作 给你两个整数，n 和 start 。
    数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
    
    请返回 nums 中所有元素按位异或（XOR）后得到的结果。

---
+++ 1400/test1.py
@@ -0,0 +1,9 @@
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# nums = [1, 2, 3, 4, 5]
+# nums = [1, 2, 3, 4, 5, 6, 7, 8]
+nums = [1, 2, 3, 4, 5, 6 ]
+result=0
+for num in nums:
+    result=result^num
+
+print(result)
