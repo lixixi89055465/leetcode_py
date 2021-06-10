86479ba8cc225a2044aad249a21866dcd2ce599b
lixiang <1850094299@qq.com>
2021-06-10 13:30:51 +0800

    518 背包

---
+++ 500/518_change_medium.py
@@ -0,0 +1,66 @@
+'''
+518. 零钱兑换 II
+给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+
+
+
+示例 1:
+
+输入: amount = 5, coins = [1, 2, 5]
+输出: 4
+解释: 有四种方式可以凑成总金额:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+示例 2:
+
+输入: amount = 3, coins = [2]
+输出: 0
+解释: 只用面额2的硬币不能凑成总金额3。
+示例 3:
+
+输入: amount = 10, coins = [10]
+输出: 1
+
+
+注意:
+
+你可以假设：
+
+0 <= amount (总金额) <= 5000
+1 <= coin (硬币面额) <= 5000
+硬币种类不超过 500 种
+结果符合 32 位符号整数
+
+'''
+
+
+class Solution:
+    def change(self, amount: int, coins: list) -> int:
+        if amount==0:
+            return 1
+        dp = [[0 for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
+        for i in range(len(coins)):
+            for j in range(1, amount + 1):
+                for k in range(j // coins[i]+1):
+                    dp[i + 1][j] += dp[i][j - k * coins[i]]
+                if j % coins[i] == 0:
+                    dp[i + 1][j] += 1
+        return dp[-1][-1]
+
+
+solve = Solution()
+# amount = 17
+# coins = [5, 7]
+# amount = 5
+# coins = [1, 2, 3, 5]
+# amount = 3
+# coins = [2]
+# amount = 10
+# coins = [10]
+
+amount = 0
+coins = [7]
+result=solve.change(amount, coins)
+print(result)
