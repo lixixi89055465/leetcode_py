ec7c162a1c20d60f6295025b9f7c9c024a655282
lixiang <1850094299@qq.com>
2021-07-07 10:34:21 +0800

    1711. 大餐计数

--- 1700/test1.py
+++ 1700/test1.py
@@ -1,8 +1,31 @@
+import collections
+import math
+class Solution:
+    def countPairs(self, deliciousness: list) -> int:
+        Hash = collections.defaultdict(int)
+        ans = 0
+        Max = int(math.log2(max(deliciousness)))+2
+        mod = 10**9 + 7
+        def gethash(num):
+            Sum = 0
+            begin =  0 if num == 0 else int(math.log2(num))
+            for i in range(begin,Max):
+                if 2**i - num in Hash:
+                    # print("num",num," Hash",2**i - num,"sum",Sum)
+                    Sum += Hash[2**i - num]%mod
+            return Sum%mod
+        for i in deliciousness:
+            temp = gethash(i)
+            ans += temp
+            ans %= mod
+            Hash[i] += 1
+        return ans%mod
 
-total=0
-for i in range(5):
-    total^=i
 
-print(total)
-print(1^2^3^4)
-print(1^2^3^4^5)
+solve = Solution()
+# deliciousness = [1, 1, 1, 3, 3, 3, 7]
+# deliciousness = [1, 3, 5, 7, 9]
+# deliciousness = [32 for _ in range(300000)]
+deliciousness = [32 for _ in range(300001)]
+result = solve.countPairs(deliciousness)
+print(result)
