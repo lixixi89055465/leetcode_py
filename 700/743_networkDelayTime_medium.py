aae158dfd327b5aefe463331a77d7f76dba20a6e
lixiang <1850094299@qq.com>
2021-08-02 11:09:23 +0800

    743. 网络延迟时间

---
+++ 700/743_networkDelayTime_medium.py
@@ -0,0 +1,53 @@
+'''
+743. 网络延迟时间
+有 n 个网络节点，标记为 1 到 n。
+
+给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， wi 是一个信号从源节点传递到目标节点的时间。
+
+现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回 -1 。
+
+
+
+示例 1：
+
+
+
+输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+输出：2
+示例 2：
+
+输入：times = [[1,2,1]], n = 2, k = 1
+输出：1
+示例 3：
+
+输入：times = [[1,2,1]], n = 2, k = 2
+输出：-1
+'''
+
+
+class Solution:
+    def networkDelayTime(self, times, n: int, k: int) -> int:
+        g = [[float('inf')] * n for _ in range(n)]
+        for x, y, time in times:
+            g[x - 1][y - 1] = time
+        dist = [float('inf')] * n
+        dist[k - 1] = 0
+        used = [False] * n
+        for _ in range(n):
+            x = -1
+            for y, u in enumerate(used):
+                if not u and (x == -1 or dist[y] < dist[x]):
+                    x = y
+            used[x] = True
+            for y, time in enumerate(g[x]):
+                dist[y] = min(dist[y], dist[x] + time)
+        ans = max(dist)
+        return ans if ans < float('inf') else -1
+
+
+solve = Solution()
+times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
+n = 4
+k = 2
+result = solve.networkDelayTime(times, n, k)
+print(result)
