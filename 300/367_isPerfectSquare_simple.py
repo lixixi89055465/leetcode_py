483488c9bade3b06c0360058c69c7dd169417796
lixixi89055465 <nanji1850094299@outlook.com>
2021-11-19 09:33:11 +0800

    367. 有效的完全平方数

---
+++ 300/367_isPerfectSquare_simple.py
@@ -0,0 +1,39 @@
+'''
+367. 有效的完全平方数
+给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+
+进阶：不要 使用任何内置的库函数，如  sqrt 。
+
+
+
+示例 1：
+
+输入：num = 16
+输出：true
+示例 2：
+
+输入：num = 14
+输出：false
+
+'''
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left, right = 0, num
+        while left <= right:
+            mid = (left + right) // 2
+            square = mid * mid
+            if square < num:
+                left = mid + 1
+            elif square > num:
+                right = mid - 1
+            else:
+                return True
+        return False
+
+
+solve = Solution()
+n = 16
+result = solve.isPerfectSquare(n)
+print(result)
