5687efb8edb23d28629f8647245fb022c4a4e62e
lixixi89055465 <nanji1850094299@outlook.com>
2021-11-26 14:23:29 +0800

    22. 括号生成

--- 000/022_generateParenthesis_medium.py
+++ 000/022_generateParenthesis_medium.py
@@ -21,8 +21,29 @@
 1 <= n <= 8
 '''
 
+
 class Solution:
-    def generateParenthesis(self, n: int) -> list[str]:
-        pass
-solve=Solution()
-solve.generateParenthesis()
+    def generateParenthesis(self, n: int):
+        ans = []
+
+        def backtrack(S, left, right):
+            if len(S) == n * 2:
+                ans.append(''.join(S))
+                return
+            if left < n:
+                S.append('(')
+                backtrack(S, left + 1, right)
+                S.pop()
+            if right < left:
+                S.append(')')
+                backtrack(S, left, right + 1)
+                S.pop()
+
+        backtrack([], 0, 0)
+        return ans
+
+
+solve = Solution()
+n = 4
+result = solve.generateParenthesis(n)
+print(result)
