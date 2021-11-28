d53325ccfe2b487cea5f2880e44194c89f457983
lixixi89055465 <nanji1850094299@outlook.com>
2021-11-28 11:34:27 +0800

    32. 最长有效括号

--- 000/032_longestValidParentheses_hard.py
+++ 000/032_longestValidParentheses_hard.py
@@ -29,10 +29,34 @@ s[i] 为 '(' 或 ')'
 
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        pass
+        left, right = 0, 0
+        maxL = 0
+        for i in s:
+            if i == '(':
+                left += 1
+            elif i == ')':
+                right += 1
+                if right == left:
+                    maxL = max(maxL, 2 * left)
+                elif right > left:
+                    left, right = 0, 0
+        left, right = 0, 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '(':
+                left += 1
+                if left == right:
+                    maxL = max(maxL, 2 * right)
+                elif left > right:
+                    left, right = 0, 0
+            elif s[i] == ')':
+                right += 1
+        return maxL
 
 
 solve = Solution()
-s = "(()"
+s = ")()())"
+# s = "(()"
+# s = ""
+s = "()"
 result = solve.longestValidParentheses(s)
 print(result)
