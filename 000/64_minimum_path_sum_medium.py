84040cd172ca573e7b68371d0f7eca18308405da
lixixi89055465 <nanji1850094299@outlook.com>
2021-12-04 11:20:48 +0800

    64. 最小路径和

--- 000/64_minimum_path_sum_medium.py
+++ 000/64_minimum_path_sum_medium.py
@@ -1,5 +1,29 @@
 '''
-minimum path sum
+64. 最小路径和
+给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+
+说明：每次只能向下或者向右移动一步。
+
+
+
+示例 1：
+
+
+输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+输出：7
+解释：因为路径 1→3→1→1→1 的总和最小。
+示例 2：
+
+输入：grid = [[1,2,3],[4,5,6]]
+输出：12
+
+
+提示：
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 100
 '''
 
 
@@ -8,35 +32,23 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        dp = [[0 for 0 in range(n)] for _ in range(m)]
+        dp = [[0 for _ in range(n)] for _ in range(m)]
         for i in range(m):
             for j in range(n):
                 if i == 0 and j == 0:
-                    continue
-                if i == 0:
-                    grid[i][j] += grid[i][j - 1]
+                    dp[i][j] = grid[i][j]
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
                 elif j == 0:
-                    grid[i][j] += grid[i - 1][j]
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
                 else:
                     dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
 
-        return dp[m][n]
+        return dp[m-1][n-1]
 
-    def minPathSum1(self, grid):
-        M = grid.length
-        N = grid[0].length
-        memo = [[0 for _ in range(N)] for _ in range(M)]
-        return self.helper(grid, M - 1, N - 1, memo)
 
-    def helper(self, grid, i, j, memo):
-        if i == 0 and j == 0:
-            return grid[0][0]
-        if i < 0 or j < 0:
-            return 100000
-        if memo[i][j] > 0:
-            return memo[i][j]
-        return grid[i][j] + min(
-            self.helper(grid, i - 1, j, memo),
-            self.helper(grid, i, j - 1, memo)
-        )
-
+solve = Solution()
+# grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
+grid = [[1, 2, 3], [4, 5, 6]]
+result = solve.minPathSum(grid)
+print(result)
