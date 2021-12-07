8610610c4967a66d75be2d5f6b927a738d6e3953
lixixi89055465 <nanji1850094299@outlook.com>
2021-12-07 12:24:27 +0800

    84. 柱状图中最大的矩形

--- 000/084_largestRectangleArea_hard.py
+++ 000/084_largestRectangleArea_hard.py
@@ -28,6 +28,44 @@ largestRectangleArea
 0 <= heights[i] <= 104
 '''
 
+#
 class Solution:
     def largestRectangleArea(self, heights: list) -> int:
-        pass
+        s = []
+        i = 0
+        n = len(heights)
+        left, right = [0] * n, [n] * n
+        print(heights)
+        while i < n:
+            while s and heights[s[-1]] > heights[i]:
+                right[s[-1]] = i
+                s.pop()
+            left[i] = s[-1] if s else -1
+            s.append(i)
+            i += 1
+        ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+        return ans
+
+# class Solution:
+#     def largestRectangleArea(self, heights: list) -> int:
+#         n = len(heights)
+#         left, right = [0] * n, [n] * n
+#
+#         mono_stack = list()
+#         for i in range(n):
+#             while mono_stack and heights[mono_stack[-1]] >= heights[i]:
+#                 right[mono_stack[-1]] = i
+#                 mono_stack.pop()
+#             left[i] = mono_stack[-1] if mono_stack else -1
+#             mono_stack.append(i)
+#
+#         ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
+#         return ans
+
+
+solve = Solution()
+# heights = [2, 1, 5, 6, 2, 3]
+heights = [2,4]
+
+result = solve.largestRectangleArea(heights)
+print(result)
