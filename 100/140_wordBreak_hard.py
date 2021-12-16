c119887471e82729208173c49d4ef92a74b52f40
lixixi89055465 <nanji1850094299@outlook.com>
2021-12-16 11:04:12 +0800

    140. 单词拆分 II

--- 100/140_wordBreak_hard.py
+++ 100/140_wordBreak_hard.py
@@ -40,11 +40,27 @@ wordDict = ["cats", "dog", "sand", "and", "cat"]
 
 
 class Solution:
-    def wordBreak(self, s: str, wordDict):
-        pass
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        @lru_cache(None)
+        def backtrack(index: int) -> List[List[str]]:
+            if index == len(s):
+                return [[]]
+            ans = list()
+            for i in range(index + 1, len(s) + 1):
+                word = s[index:i]
+                if word in wordSet:
+                    nextWordBreaks = backtrack(i)
+                    for nextWordBreak in nextWordBreaks:
+                        ans.append(nextWordBreak.copy() + [word])
+            return ans
+
+        wordSet = set(wordDict)
+        breakList = backtrack(0)
+        return [" ".join(words[::-1]) for words in breakList]
 
 
 solve = Solution()
 s = "catsanddog"
 wordDict = ["cat", "cats", "and", "sand", "dog"]
-solve.wordBreak()
+result = solve.wordBreak(s, wordDict)
+print(result)
