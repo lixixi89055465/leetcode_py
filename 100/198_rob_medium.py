65c738b594434878833ddd3d676c40b502eb858e
lixixi89055465 <nanji1850094299@outlook.com>
2021-12-18 13:56:25 +0800

    213. 打家劫舍 II

--- 100/198_rob_medium.py
+++ 100/198_rob_medium.py
@@ -31,15 +31,18 @@
 class Solution:
     def rob(self, nums) -> int:
         n = len(nums)
-        dp = [[0, 0] for _ in range(n + 1)]
-        for i in range(1, n + 1):
-            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1])
-            dp[i][1] = dp[i - 1][0] + nums[i - 1]
-        return max(dp[-1])
+        if n <= 2:
+            return max(nums)
+        dp = [0 for _ in range(n)]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[-1]
 
 
 solve = Solution()
-# nums = [2, 7, 9, 3, 1]
-nums = [1, 2, 3, 1]
+nums = [2, 7, 9, 3, 1]
+# nums = [1, 2, 3, 1]
 result = solve.rob(nums)
 print(result)
