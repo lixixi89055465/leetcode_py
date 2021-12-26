1e16d2328ff5009faeedaf58f3c72aa6019edd7f
lixixi89055465 <nanji1850094299@outlook.com>
2021-12-26 12:45:18 +0800

    309. 最佳买卖股票时机含冷冻期

--- 300/309_maxProfit_medium.py
+++ 300/309_maxProfit_medium.py
@@ -14,9 +14,16 @@
 '''
 
 
+# 0买完     1卖完      2空手
 class Solution:
     def maxProfit(self, prices) -> int:
-        pass
+        dp = [[0, 0, 0] for _ in range(len(prices))]
+        dp[0] = [-prices[0], 0, 0]
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][2] - prices[i])
+            dp[i][1] = dp[i - 1][0]+ prices[i]
+            dp[i][2] = max(dp[i - 1][1], dp[i - 1][2])
+        return max(dp[-1])
 
 
 solve = Solution()
