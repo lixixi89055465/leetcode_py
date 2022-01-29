d964db6b29115694fdd8fda0479b96b2c20b6159
nanji <1850094299@qq.com>
2022-01-29 15:44:09 +0800

    1765. 地图中的最高点

--- base/test1.py
+++ base/test1.py
@@ -1,32 +1,5 @@
-class A:
-    def aa(self):
-        print('aa')
-
-    def say(self):
-        print('say AAA!')
-
-
-class B:
-    def bb(self):
-        print('bb')
-
-    def say(self):
-        print('say BBB!')
-
-
-class C(B, A):
-    def __init__(self, nn):
-        self.nn = nn
-
-    def cc(self):
-        print('CC')
-
-
-c = C(3)
-c.say()
-print(dir(c))
-print(c.__dict__)
-print(c.__class__)
-# print(c.__bases__)
-# print(c.__base__)
-print(C.mro())
+import sys
+list1=sys.path
+print(type(list1))
+for path in list1:
+    print(path)
