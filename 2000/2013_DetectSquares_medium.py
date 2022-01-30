19cdd6a06cea94ace41651e0a120a6fb13b4a358
nanji <1850094299@qq.com>
2022-01-30 23:32:52 +0800

    2013. 检测正方形

--- 2000/2013_DetectSquares_medium.py
+++ 2000/2013_DetectSquares_medium.py
@@ -53,25 +53,28 @@ class DetectSquares:
     def __init__(self):
         self.map = defaultdict(Counter)
 
-    def add(self, point: list[int]) -> None:
+    def add(self, point):
         x, y = point
         self.map[y][x] += 1
 
-    def count(self, point: list[int]) -> int:
+    def count(self, point):
         res = 0
         x, y = point
         if not y in self.map:
             return 0
         yCnt = self.map[y]
-        for col, colCnt in self.map.items():
+        for col, xCnt in self.map.items():
             if col != y:
-                d = col - y
-                res+=colCnt[x]*yCnt[x+d]*colCnt[x+d]
-                res+=colCnt[x]*yCnt[x-d]*colCnt[x-d]
+                d = y - col
+                res+=xCnt[x]*yCnt[x-d]*yCnt[x+d]
+                res+=xCnt[x]*yCnt[x+d]*yCnt[x-d]
         return res
 
-obj = DetectSquares()
-point = [3, 10]
-obj.add(point)
-param_2 = obj.count(point)
-print(param_2)
+
+detectSquares = DetectSquares()
+detectSquares.add([3, 10])
+detectSquares.add([11, 2])
+detectSquares.add([3, 2])
+print(detectSquares.map)
+result = detectSquares.count([11, 10])
+print(result)
