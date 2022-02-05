47f57934178f6a3a81bd8205994b91538fbdb0c4
nanji <1850094299@qq.com>
2022-02-05 11:27:14 +0800

    1219. 黄金矿工

--- 2000/2045_secondMinimum_hard.py
+++ 2000/2045_secondMinimum_hard.py
@@ -66,37 +66,32 @@ from collections import deque
 
 class Solution:
     def secondMinimum(self, n: int, edges, time, change) -> int:
+        distances = [float('inf')] * (n + 1)
+        distances[1]=0
         graph = [[] for _ in range(n + 1)]
-        for e in edges:
-            x, y = e[0], e[1]
-            graph[x].append(y)
-            graph[y].append(x)
-        # dist[i][0] 表示从1到i 的最短路径长度，dist[i][1]表示从1到i的严格次短路径
-        dist = [[float('inf')] * 2 for _ in range(n + 1)]
-        dist[1][0] = 0
+        for t1, t2 in edges:
+            graph[t1].append(t2)
+            graph[t2].append(t1)
         q = deque([(1, 0)])
-        while dist[n][1] == float('inf'):
-            p = q.popleft()
-            for y in graph[p[0]]:
-                d = p[1] + 1
-                if d < dist[y][0]:
-                    dist[y][0] = d
-                    q.append((y, d))
-                elif dist[y][0] < d < dist[y][1]:
-                    dist[y][1] = d
-                    q.append((y, d))
-        ans = 0
-        for _ in range(dist[n][1]):
-            if ans % (change * 2) >= change:
-                ans += change * 2 - ans % (change * 2)
-            ans += time
-        return ans
+        while distances[-1] == float('inf'):
+            x, y = q.popleft()
+            for i in graph[x]:
+                d = y + 1
+                if d < distances[i]:
+                    distances[i] = d
+                    q.append((i, d))
+        print(distances)
 
 
 solve = Solution()
-n = 2
-edges = [[1, 2]]
+# n = 2
+# edges = [[1, 2]]
+# time = 3
+# change = 2
+n = 5
+edges = [[1, 2], [1, 3], [1, 4], [3, 4], [4, 5]]
 time = 3
-change = 2
+change = 5
 result = solve.secondMinimum(n, edges, time, change)
+
 print(result)
