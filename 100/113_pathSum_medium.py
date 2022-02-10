5b1b979111bd8c92bb161e87e00b0c5737e49e1b
nanji <1850094299@qq.com>
2022-02-10 16:36:14 +0800

    113. 路径总和 II

--- 100/113_pathSum_medium.py
+++ 100/113_pathSum_medium.py
@@ -40,37 +40,31 @@ class TreeNode:
 
 
 class Solution:
-    def pathSum(self, root, targetSum) -> bool:
-        ans = []
-
-        def helper(node, targetS, S):
-            if not node:
-                return False
-            if node.val == targetS and not (node.left or node.right):
-                S.append(node.val)
-                ans.append(S[:])
-                S.pop()
-                return True
-
-            S.append(node.val)
-            left = helper(node.left, targetS - node.val, S)
-            S.pop()
-            S.append(node.val)
-            right = helper(node.right, targetS - node.val, S)
-            S.pop()
-            return left or right
-
+    def flatten(self, root: TreeNode) -> None:
         if not root:
             return []
-        helper(root, targetSum, [])
-        return ans
+        q = deque()
+        ans = []
+        while q or root:
+            while root:
+                q.append(root)
+                ans.append(root)
+                root = root.left
+            root = q.pop()
+            root = root.right
+        root = ans[0]
+        root.right = None
+        for i in ans[1:]:
+            root.left = i
+            i.right = None
+            root = i
+        return ans[0]
 
 
 from collections import deque
 
 solve = Solution()
-root = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, 5, 1]
-targetSum = 22
+root = [1, 2, 5, 3, 4, None, 6]
 q = deque(root)
 s = deque()
 r1 = TreeNode(q.popleft())
@@ -85,13 +79,12 @@ while s and q:
         s.append(a1.left)
     else:
         s.append(None)
-    if q:
-        tmp1 = q.popleft()
-        if tmp1:
-            a1.right = TreeNode(tmp1)
-            s.append(a1.right)
-        else:
-            s.append(None)
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.right = TreeNode(tmp1)
+        s.append(a1.right)
+    else:
+        s.append(None)
 
-result = solve.pathSum(r1, targetSum)
+result = solve.flatten(r1)
 print(result)
