a7dba567e8fb07ffcb87f93c7737c99b62775cec
nanji <1850094299@qq.com>
2022-02-12 12:17:30 +0800

    230. 二叉搜索树中第K小的元素

---
+++ 200/230_kthSmallest_medium.py
@@ -0,0 +1,86 @@
+'''
+230. 二叉搜索树中第K小的元素
+给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。
+
+
+
+示例 1：
+
+
+输入：root = [3,1,4,null,2], k = 1
+输出：1
+示例 2：
+
+
+输入：root = [5,3,6,2,4,null,null,1], k = 3
+输出：3
+
+
+
+
+提示：
+
+树中的节点数为 n 。
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+
+
+进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from collections import deque
+
+
+class Solution:
+    def kthSmallest(self, root, k):
+        if not root:
+            return 0
+        s = deque()
+        while s or root:
+            while root:
+                s.append(root)
+                root = root.left
+            root = s.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+
+
+solve = Solution()
+root = [3, 1, 4, None, 2]
+k = 4
+q = deque(root)
+s = deque()
+r1 = TreeNode(q.popleft())
+s.append(r1)
+while s and q:
+    a1 = s.popleft()
+    while not a1:
+        a1 = s.popleft()
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.left = TreeNode(tmp1)
+        s.append(a1.left)
+    else:
+        s.append(None)
+    if not q:
+        break
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.right = TreeNode(tmp1)
+        s.append(a1.right)
+    else:
+        s.append(None)
+
+result = solve.kthSmallest(r1, k)
+print(result)
