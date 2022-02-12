9783ae5d6d622392340be460db9ad411baacabb6
nanji <1850094299@qq.com>
2022-02-12 11:36:38 +0800

    226. 翻转二叉树

---
+++ 200/226_invertTree_simple.py
@@ -0,0 +1,81 @@
+'''
+226. 翻转二叉树
+给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+
+
+
+示例 1：
+
+
+
+输入：root = [4,2,7,1,3,6,9]
+输出：[4,7,2,9,6,3,1]
+示例 2：
+
+
+
+输入：root = [2,1,3]
+输出：[2,3,1]
+示例 3：
+
+输入：root = []
+输出：[]
+
+
+提示：
+
+树中节点数目范围在 [0, 100] 内
+-100 <= Node.val <= 100
+
+'''
+
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
+solve = Solution()
+root = [4, 2, 7, 1, 3, 6, 9]
+q = deque(root)
+s = deque()
+r1 = TreeNode(q.popleft())
+s.append(r1)
+while s and q:
+    a1 = s.popleft()
+    while not a1:
+        a1 = s.popleft()
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.left = TreeNode(tmp1)
+        s.append(a1.left)
+    else:
+        s.append(None)
+    if not q:
+        break
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.right = TreeNode(tmp1)
+        s.append(a1.right)
+    else:
+        s.append(None)
+
+result = solve.invertTree(r1)
+print(result)
