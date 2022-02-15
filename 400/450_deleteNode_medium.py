44d6d503b944573867bb84aca7a0e1b2ddf39337
nanji <1850094299@qq.com>
2022-02-15 16:11:51 +0800

    450. 删除二叉搜索树中的节点

---
+++ 400/450_deleteNode_medium.py
@@ -0,0 +1,129 @@
+'''
+450. 删除二叉搜索树中的节点
+给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的根节点的引用。
+
+一般来说，删除节点可分为两个步骤：
+
+首先找到需要删除的节点；
+如果找到了，删除它。
+
+
+示例 1:
+
+
+
+输入：root = [5,3,6,2,4,null,7], key = 3
+输出：[5,4,6,2,null,null,7]
+解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+另一个正确答案是 [5,2,6,null,4,null,7]。
+
+
+示例 2:
+
+输入: root = [5,3,6,2,4,null,7], key = 0
+输出: [5,3,6,2,4,null,7]
+解释: 二叉树不包含值为 0 的节点
+示例 3:
+
+输入: root = [], key = 0
+输出: []
+
+
+提示:
+
+节点数的范围 [0, 104].
+-105 <= Node.val <= 105
+节点值唯一
+root 是合法的二叉搜索树
+-105 <= key <= 105
+
+
+进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def successor(self, root):
+        """
+        One step right and then always left
+        """
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+
+    def predecessor(self, root):
+        """
+        One step left and then always right
+        """
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+
+        # delete from the right subtree
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        # delete from the left subtree
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        # delete the current node
+        else:
+            # the node is a leaf
+            if not (root.left or root.right):
+                root = None
+            # the node is not a leaf and has a right child
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            # the node is not a leaf, has no right child, and has a left child
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+
+        return root
+
+
+
+from collections import deque
+
+root = [5, 2, 6, None, 4, None, 7]
+key = 2
+q = deque(root)
+s = deque()
+r1 = TreeNode(q.popleft())
+s.append(r1)
+while s and q:
+    a1 = s.popleft()
+    while not a1:
+        a1 = s.popleft()
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.left = TreeNode(tmp1)
+        s.append(a1.left)
+    else:
+        s.append(None)
+    if not q:
+        break
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.right = TreeNode(tmp1)
+        s.append(a1.right)
+    else:
+        s.append(None)
+solve = Solution()
+result = solve.deleteNode(r1, key)
+print(result)
