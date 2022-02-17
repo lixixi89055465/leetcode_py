07a1bf579df771ac0e34ab93b0ffd5a031ebce63
nanji <1850094299@qq.com>
2022-02-17 14:31:53 +0800

    530. 二叉搜索树的最小绝对差

--- 500/530_getMinimumDifference_simple.py
+++ 500/530_getMinimumDifference_simple.py
@@ -24,3 +24,61 @@
 0 <= Node.val <= 105
 
 '''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+from collections import deque
+
+
+class Solution:
+    def getMinimumDifference(self, root: TreeNode) -> int:
+        ans = []
+        q = deque()
+        while q or root:
+            while root:
+                q.append(root)
+                root = root.left
+            p = q.pop()
+            ans.append(p.val)
+            if p.right:
+                root = p.right
+        alen = len(ans)
+        mAns = float('inf')
+        for i in range(alen - 1):
+            mAns = min(mAns, abs(ans[i] - ans[i + 1]))
+        return mAns
+
+
+root = [236, 104, 701, None, 227, None, 911]
+q = deque(root)
+s = deque()
+r1 = TreeNode(q.popleft())
+s.append(r1)
+while s and q:
+    a1 = s.popleft()
+    while not a1:
+        a1 = s.popleft()
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.left = TreeNode(tmp1)
+        s.append(a1.left)
+    else:
+        s.append(None)
+    if not q:
+        break
+    tmp1 = q.popleft()
+    if tmp1:
+        a1.right = TreeNode(tmp1)
+        s.append(a1.right)
+    else:
+        s.append(None)
+solve = Solution()
+result = solve.getMinimumDifference(r1)
+print(result)
