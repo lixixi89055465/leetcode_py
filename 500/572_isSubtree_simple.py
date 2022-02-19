53fab59926eed26971460174bf9d721485ef7a03
nanji <1850094299@qq.com>
2022-02-19 11:33:16 +0800

    1. 两数之和

--- 500/572_isSubtree_simple.py
+++ 500/572_isSubtree_simple.py
@@ -26,12 +26,84 @@ subRoot 树上的节点数量范围是 [1, 1000]
 -104 <= subRoot.val <= 104
 '''
 
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def isSubtree(self, root: TreeNode, subRoot: TreeNode) -> bool:
-        pass
+        def getAll(node):
+            q = deque()
+            rArr = []
+            while q or node:
+                while node:
+                    q.append(node)
+                    node = node.left
+                p = q.pop()
+                rArr.append(p)
+                if p.right:
+                    node = p.right
+            return rArr
+
+        rArr = getAll(root)
+        sArr = getAll(subRoot)
+        for i in range(len(rArr) - len(sArr) + 1):
+            index = 0
+            for j in range(len(sArr)):
+                if rArr[i + j].val == sArr[j].val:
+                    index += 1
+                    continue
+                break
+
+            if index == len(sArr):
+                return True
+        return False
+
+
+from collections import deque
+
+
+def getRoot(node):
+    q = deque(node)
+    s = deque()
+    r1 = TreeNode(q.popleft())
+    s.append(r1)
+    while s and q:
+        a1 = s.popleft()
+        while not a1:
+            a1 = s.popleft()
+        tmp1 = q.popleft()
+        if tmp1:
+            a1.left = TreeNode(tmp1)
+            s.append(a1.left)
+        else:
+            s.append(None)
+        if not q:
+            break
+        tmp1 = q.popleft()
+        if tmp1:
+            a1.right = TreeNode(tmp1)
+            s.append(a1.right)
+        else:
+            s.append(None)
+    return r1
+
+
+r1 = [3, 4, 5, 1, 2]
+r2 = [4, 1, 2]
+# r1 = [3, 4, 5, 1, 2, None, None, None, None, 1]
+# r2 = [4, 1, 2]
+r1 = [1, 2, 3]
+r2 = [1, 2]
+# r1 = [1]
+# r2 = [1]
+solve = Solution()
+r1 = getRoot(r1)
+r2 = getRoot(r2)
+result = solve.isSubtree(r1, r2)
+print(result)
