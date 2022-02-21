6fc19ae1b92e42d96bb312358dd414020d1e7573
nanji <1850094299@qq.com>
2022-02-21 13:30:31 +0800

    49. 字母异位词分组

---
+++ 000/049_groupAnagrams_medium.py
@@ -0,0 +1,48 @@
+'''
+49. 字母异位词分组
+给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+
+字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+
+
+
+示例 1:
+
+输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+示例 2:
+
+输入: strs = [""]
+输出: [[""]]
+示例 3:
+
+输入: strs = ["a"]
+输出: [["a"]]
+
+
+提示：
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] 仅包含小写字母
+
+'''
+
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        m = defaultdict(list)
+        for s in strs:
+            counts = [0] * 26
+            for ch in s:
+                counts[ord(ch) - ord('a')] += 1
+            m[tuple(counts)].append(s)
+        return list(m.values())
+
+
+solve = Solution()
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+result = solve.groupAnagrams(strs)
+print(result)
