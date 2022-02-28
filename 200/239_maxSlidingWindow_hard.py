151e77ac1bb4517a3c29b018024ef3240caefcc1
nanji <1850094299@qq.com>
2022-02-28 14:57:03 +0800

    239. 滑动窗口最大值

---
+++ 200/239_maxSlidingWindow_hard.py
@@ -0,0 +1,67 @@
+'''
+239. 滑动窗口最大值
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回 滑动窗口中的最大值 。
+
+
+
+示例 1：
+
+输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+输出：[3,3,5,5,6,7]
+解释：
+滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+示例 2：
+
+输入：nums = [1], k = 1
+输出：[1]
+
+
+提示：
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+'''
+
+import collections
+
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        n = len(nums)
+        q = collections.deque()
+        for i in range(k):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+
+        ans = [nums[q[0]]]
+        for i in range(k, n):
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            q.append(i)
+            while q[0] <= i - k:
+                q.popleft()
+            ans.append(nums[q[0]])
+
+        return ans
+
+
+solve = Solution()
+# nums = [1, 3, 1, 5, 100]
+# k = 3
+# nums = [1, -1]
+# k = 1
+nums = [1, 3, 1, 2, 0, 5]
+k = 3
+result = solve.maxSlidingWindow(nums, k)
+print(result)
