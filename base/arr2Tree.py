106e8ef8f391c550c34610ee65cf6d136a76273d
nanji <1850094299@qq.com>
2022-09-01 16:46:19 +0800

    998. 最大二叉树 II

--- base/arr2Tree.py
+++ base/arr2Tree.py
@@ -11,24 +11,53 @@ class TreeNode:
 targetSum = 22
 
 root = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1]
-q = deque(root)
-s = deque()
-r1 = TreeNode(q.popleft())
-s.append(r1)
-while s and q:
-    a1 = s.popleft()
-    while not a1:
+# q = deque(root)
+# s = deque()
+# r1 = TreeNode(q.popleft())
+# s.append(r1)
+# while s and q:
+#     a1 = s.popleft()
+#     while not a1:
+#         a1 = s.popleft()
+#     tmp1 = q.popleft()
+#     if tmp1:
+#         a1.left = TreeNode(tmp1)
+#         s.append(a1.left)
+#     else:
+#         s.append(None)
+#     tmp1 = q.popleft()
+#     if tmp1:
+#         a1.right = TreeNode(tmp1)
+#         s.append(a1.right)
+#     else:
+#         s.append(None)
+# root = r1
+
+def arrToTree(arr):
+    from collections import deque
+    q = deque(arr)
+    s = deque()
+    r1 = TreeNode(q.popleft())
+    s.append(r1)
+    while s and q:
         a1 = s.popleft()
-    tmp1 = q.popleft()
-    if tmp1:
-        a1.left = TreeNode(tmp1)
-        s.append(a1.left)
-    else:
-        s.append(None)
-    tmp1 = q.popleft()
-    if tmp1:
-        a1.right = TreeNode(tmp1)
-        s.append(a1.right)
-    else:
-        s.append(None)
-root = r1
+        while not a1:
+            a1 = s.popleft()
+        tmp1 = q.popleft()
+        if tmp1:
+            a1.left = TreeNode(tmp1)
+            s.append(a1.left)
+        else:
+            s.append(None)
+        tmp1 = q.popleft()
+        if tmp1:
+            a1.right = TreeNode(tmp1)
+            s.append(a1.right)
+        else:
+            s.append(None)
+    root = r1
+    return root
+
+root = [5, 4, 8, 11, None, 13, 4, 7, 2, None, None, None, 1]
+ans=arrToTree(root)
+print(ans)
