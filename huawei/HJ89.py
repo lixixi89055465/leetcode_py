ab3f4a23d8991dfcf8d58d80f59b3624ce6f054b
lixixi89055465 <nanji1850094299@outlook.com>
2023-06-15 11:54:36 +0800

    huawei nowcoder

--- huawei/HJ89.py
+++ huawei/HJ89.py
@@ -1,43 +1,64 @@
 import sys
 
-m = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13}
+m = {'A': 1, 'B': '2', '2':2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12,
+     'K': 13}
+reverse_m = {1: 'A', 2: '2', 11: 'J', 12: 'Q', 13: 'K'}
+
+
+def process02(list, s, index):
+    if index == 0:
+        tmp = process02(list, list[0], 1)
+        return [list[0]] + tmp if tmp != None else None
+    if index == 4:
+        return [] if s == 24 else None
+    fu = ['+', '-', '*', '/']
+    for f1 in fu:
+        tmp = s
+        if f1 == '+':
+            tmp += list[index]
+        elif f1 == '-':
+            tmp -= list[index]
+        elif f1 == '*':
+            tmp *= list[index]
+        else:
+            tmp //= list[index]
+        resArr = process02(list, tmp, index + 1)
+        if resArr != None:
+            resArr = [f1, list[index]] + resArr
+            return resArr
+    return None
 
 
 def process01(list):
     res = []
-    for e in list:
-        tmp = process01(list.remove(e))
-        for t in tmp:
-            t = e + t;
-        res.append(tmp)
-    return res
-
-
-fu = ['+', '-', '*', '/']
+    for i in range(0, 4):
+        for j in range(0, 4):
+            for k in range(0, 4):
+                for t in range(0, 4):
+                    if i != j and i != k and i != t and j != k and j != t and k != t:
+                        tmp = process02([list[i], list[j], list[k], list[t]], 0, 0)
+                        if tmp != None:
+                            return tmp
+    return None
 
 
 def process(arr):
     list = []
     for e in arr:
-        list.append(m[e])
-    ans = process01(list)
-    res=[]
-    for l in ans:
-        for f1 in fu:
-            for f2 in fu:
-                for f3 in fu:
-                    if f1.equ
-
-
-
-print('start')
-
-for line in sys.stdin:
-    a = line.split()
-    print('0' * 100)
-
-    for i in a:
-        if len(i) > 2:
+        if len(e) > 2:
             print('ERROR')
-            exit(0)
-    process(a)
+            return
+        list.append(m[e])
+    tmp = process01(list)
+    if tmp != None:
+        print(''.join(str(i) if i not in reverse_m else str(reverse_m[i]) for i in tmp))
+    else:
+        print('NONE')
+
+
+# a = '4 2 K A'
+for line in sys.stdin:
+    a = line
+    process(a.split())
+# a = 'A A A A'
+# a = 'B 5 joker 4'
